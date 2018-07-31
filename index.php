77b9adfe9d7bd1bfe4aa3e41ab4ea79032764603
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-31 16:18:05 +0200

    exo1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <p class="date">
+      Nous sommes le <?php echo date('d / m / y'); ?>
+   </p>
+
+  </body>
+</html>
